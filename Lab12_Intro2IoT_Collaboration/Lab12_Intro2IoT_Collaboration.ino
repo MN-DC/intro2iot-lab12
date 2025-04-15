66b5be5fbe2ac0c5d309652ca6d8159b5c39551b
MN-DC <michael.nantel@dawsoncollege.qc.ca>
2025-04-15 10:38:47 -0400

    Update Lab12_Intro2IoT_Collaboration.ino

--- Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
+++ Lab12_Intro2IoT_Collaboration/Lab12_Intro2IoT_Collaboration.ino
@@ -50,10 +50,13 @@ bool isButtonActivated() {
   Serial.print("Button: ");
 
   // TODO: Fix this logic and print the correct status
-  if (state == HIGH) {
-    // Button not pressed
+  if (buttonPin == LOW) {
+    serial.print("OFF");
+    digitalwrite(ledPin, HIGH);
     return false;
   } else {
+    serial.print("ON");
+    digitalwrite(ledPin, LOW);
     // Button pressed
     return true;
   }
